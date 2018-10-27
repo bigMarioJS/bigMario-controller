42051d0702b64b05ae6732e80043c928dd54b4b7
mcbrided <dustin.mcbride@agilysys.com>
2018-10-26 21:10:08 -0700

    changed arduino software

--- src/inputs/Board.js
+++ src/inputs/Board.js
@@ -1,50 +1,118 @@
-import five from 'johnny-five';
+const SerialPort = require('serialport')
+const Delimiter = require('@serialport/parser-delimiter')
+const Ready = require('@serialport/parser-ready')
+
 import Logger from '../utils/Logger';
 const timeout = ms => new Promise(res => setTimeout(res, ms))
 
 const logger = new Logger();
 
-
 export default class Board {
-  constructor (sensorData) {
+  constructor(sensorData) {
     this.board = null;
     this.sensorData = sensorData;
+    this.sensorReady = null;
 
     this.init = this.init.bind(this)
+    this.listener = this.listener.bind(this)
   }
 
-  async init () {
-    let sensorData = this.sensorData
+  async init() {
+    logger.info('Intializing Arduino board...')
 
-    let boardInited, sensorInited;
+    while (!this.board) {
 
-    this.board = new five.Board({
-      repl: false,
-      debug: false,
-    });
+      let allSerialPorts;
 
-    this.board.on("ready", function() {
-      boardInited = true;
-      logger.info('Board ready waiting for sensor data')
+      try {
+        allSerialPorts = await SerialPort.list()
+      } catch (ex) {
+        logger.error('Cannot get system serial ports, board init failed')
+      }
 
-      let sensor = new five.Multi({
-        controller: "HTU21D"
-      });
+      let arduinoPort = allSerialPorts.filter(port => port.manufacturer === 'Arduino LLC')
 
-      sensor.on("change", function() {
-        sensorData.setData({
-          relativeHumidityOne: this.hygrometer.relativeHumidity,
-          tempatureCelsiusOne: this.thermometer.celsius
-        })
-      })
-    });
-
-    while (!boardInited & !this.sensorData) {
-      await timeout(2000);
-      if (boardInited & sensorInited) {
-        logger.info('Board fully intializing.')
-        return true;
+      if (arduinoPort.length === 0) {
+        logger.error('Arduino board not connected')
+        await timeout(1000)
+      } else {
+        logger.info('Board found waiting for sensor data')
+        let port = arduinoPort[0]
+        this.board = new SerialPort(port.comName);
       }
     }
+
+    this.listener();
+
+    console.log(this.board)
+
+    while (!this.sensorReady) {
+      await timeout(1000)
+    }
+
+    return true
+
+  }
+
+  listener() {
+    const reader = this.board.pipe(new Delimiter({
+      delimiter: '\r\n'
+    }))
+
+    reader.on('close', () => {
+        console.log('closed')
+      })
+
+      reader.on('error', () => {
+        console.log('error')
+      })
+
+    reader.on('data', (data) => {
+      let parsedJson
+
+      try {
+        parsedJson = JSON.parse(data)
+      } catch {
+        console.log('bad data')
+        parsedJson = null;
+      }
+
+      if (parsedJson) {
+        console.log('-------', parsedJson)
+        this.sensorReady = true;
+        this.sensorData.setData({
+          relativeHumidityOne: parsedJson.readings.relativeHumidity,
+          tempatureCelsiusOne: parsedJson.readings.celsius
+        })
+      }
+    })
   }
 }
+
+
+
+// let serialPort;
+
+
+// let findPort = function () {
+//   SerialPort.list().then(ports => {
+//     return arduinoPort = ports.map(port => {
+//       if (port.manufacturer === 'Arduino LLC') {
+//         console.log('prot found', port)
+//       }
+//     })
+//   })
+// }
+
+// findPort();
+
+
+// const reader = port.pipe(new Delimiter({ delimiter: '\r\n' }))
+// reader.on('data', (data) => {
+//   // check for good json
+//   console.log(data.toString())
+// })
+
+// reader.on('close', () => {
+//   console.log('closed')
+// })
