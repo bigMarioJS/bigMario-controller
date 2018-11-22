263bd71308dad2a0b0c003ccd1a4a178b93bbe13
mcbrided <dustin.mcbride@agilysys.com>
2018-11-22 14:25:36 -0800

    added logging with logz.io

--- src/utils/Logger.js
+++ src/utils/Logger.js
@@ -1,4 +1,29 @@
 import moment from 'moment'
+const stripAnsi = require('strip-ansi');
+
+
+var winston = require('winston');
+
+//var logzioWinstonTransport = require('winston-logzio');
+const LogzioWinstonTransport = require('winston-logzio');
+
+const logzioWinstonTransport = new LogzioWinstonTransport({
+  level: 'info',
+  name: 'winston_logzio',
+  token: 'NmnKlRdaLLJBMsIicXCeugjYnlYvhNrA',
+});
+
+const logger = winston.createLogger({
+  transports: [logzioWinstonTransport]
+});
+
+// var loggerOptions = {
+//     token: 'NmnKlRdaLLJBMsIicXCeugjYnlYvhNrA',
+//     host: 'listener.logz.io',
+//     type: 'YourLogType'     // OPTIONAL (If none is set, it will be 'nodejs')
+// };
+
+//winston.add(logzioWinstonTransport, loggerOptions);
 
 //TODO get real logger
 
@@ -9,20 +34,24 @@ export default class Logger {
     //this.logger = bunyan.createLogger({name: "cultivate-controller"});
   }
 
-  silly (msg) {
-    //console.log(`[${getTimeStamp()}] SILLY ${msg}`)
+  silly (message) {
+    //console.log(`[${getTimeStamp()}] SILLY ${message}`)
   }
 
-  info (msg) {
-    console.log(`[${getTimeStamp()}] INFO ${msg}`)
+  info (message) {
+    console.log(`[${getTimeStamp()}] INFO ${message}`)
+    logger.log('info', stripAnsi(message));
+
   }
 
-  warn (msg) {
-    console.log(`[${getTimeStamp()}] WARN ${msg}`)
+  warn (message) {
+    console.log(`[${getTimeStamp()}] WARN ${message}`)
+    logger.log('warn', message);
   }
 
   error (message, error) {
     //TODO log to api
     console.log(`[${getTimeStamp()}] ERROR ${message} Stack: ${error}`)
+    logger.log('warn', message, error);
   }
 }
